7d5958b4f6ee19e75a2aced0352725a4a58f0bb2
Mehrab Hossain <88287873+DevMehrab@users.noreply.github.com>
2025-04-15 10:01:57 +0600

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Nav = () => {
  return (
-    <div className="bg-zinc-900 py-6 w-screen border border-b-zinc-800">
+    <div className="bg-zinc-900 py-6 w-screen border border-b-zinc-800 absolute top-0 left-0">
       <div className="text-2xl mx-auto font-bold text-zinc-400 text-center">
        <h1> Mehrab Hossain</h1>
       </div>
